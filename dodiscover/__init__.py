41d70a567287811b73169f865d23b7a96c847143
Chris Trevino <chtrevin@microsoft.com>
2022-08-11 16:25:38 -0700

    add toolchain scaffolding
    Signed-off-by: Chris Trevino <chtrevin@microsoft.com>
    Signed-off-by: Adam Li <adam2392@gmail.com>

---
+++ dodiscover/__init__.py
@@ -0,0 +1,9 @@
+"""
+DoDiscover - a library for Python-based Causal Discovery
+"""
+
+#
+# 0.0.0 is standard placeholder for poetry-dynamic-versioning
+# any changes to this should not be checked in
+#
+__version__ = "0.0.0"
