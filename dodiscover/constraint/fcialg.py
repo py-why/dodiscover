120ac45e018b2b2b29c25808060042981165a1a5
Adam Li <adam2392@gmail.com>
2022-08-30 17:53:56 -0400

    Try again
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- dodiscover/constraint/fcialg.py
+++ dodiscover/constraint/fcialg.py
@@ -1,6 +1,6 @@
 import logging
 from itertools import combinations, permutations
-from typing import Dict, List, Optional, Set, Tuple
+from typing import List, Optional, Set, Tuple
 
 import networkx as nx
 
