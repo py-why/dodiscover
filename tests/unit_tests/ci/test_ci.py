7728d7f230f72a32e70879bdd5fbd5074a9ad44e
Adam Li <adam2392@gmail.com>
2022-09-09 15:49:55 -0400

    Fix scm error
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- tests/unit_tests/ci/test_ci.py
+++ tests/unit_tests/ci/test_ci.py
@@ -1,28 +1,11 @@
-import numpy as np
+import networkx as nx
 import pytest
-from pywhy_graphs import StructuralCausalModel
 
 from dodiscover.ci import FisherZCITest, GSquareCITest, KernelCITest, Oracle
+from dodiscover.constraint.utils import dummy_sample
 
-seed = 12345
-rng = np.random.RandomState(seed=seed)
-func_uz = lambda: rng.negative_binomial(n=1, p=0.25)
-func_uxy = lambda: rng.binomial(n=1, p=0.4)
-func_x = lambda u_xy: 2 * u_xy
-func_y = lambda x, u_xy, z: x + u_xy + z
-func_z = lambda u_z: u_z
-
-# construct the SCM and the corresponding causal graph
-scm = StructuralCausalModel(
-    exogenous={
-        "u_xy": func_uxy,
-        "u_z": func_uz,
-    },
-    endogenous={"x": func_x, "y": func_y, "z": func_z},
-)
-
-sample_df = scm.sample(n=100)
-ground_truth_graph = scm.get_causal_graph()
+ground_truth_graph = nx.DiGraph([("x", "y"), ("z", "y")])
+sample_df = dummy_sample(ground_truth_graph)
 
 
 @pytest.mark.parametrize(
