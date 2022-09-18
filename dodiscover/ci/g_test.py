c31f0a8c202941cbfb9779c7e168b8fffc8785ee
Chris Trevino <darthtrevino@users.noreply.github.com>
2022-09-17 17:07:14 -0700

    use poetry for publication (#47)
    * use poetry for publication
    * update developing docs with dynamic versioning plugin install
    * fix invalid escape sequence in g_test.py
    
    Signed-off-by: Chris Trevino <chtrevin@microsoft.com>
    Co-authored-by: Adam Li <adam2392@gmail.com>

--- dodiscover/ci/g_test.py
+++ dodiscover/ci/g_test.py
@@ -239,7 +239,7 @@ def g_square_binary(
     The ``G^2`` statistic for binary outcome 'a' and 'b' is:
 
     .. math::
-        2 \\times \sum_{a,b} S^{a,b}_{ij} ln(\\frac{s^{ab}_{ij} M}{s_i^a s_j^b})
+        2 \\times \\sum_{a,b} S^{a,b}_{ij} ln(\\frac{s^{ab}_{ij} M}{s_i^a s_j^b})
 
     which takes the sum over occurrences of 'a' and 'b' and multiplies
     it by the number of samples, M and normalizes it :footcite:`Neapolitan2003`.
