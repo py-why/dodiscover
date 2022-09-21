5b7465b40b55c87e4b04be56f73070a15e479282
Adam Li <adam2392@gmail.com>
2022-09-21 18:05:32 -0400

    [ENH] Add metrics module for comparing networks-like graphs (#48)
    * Implement confusion matrix metric for comparing two learned skeleton graphs with a unit test
    
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -126,6 +126,8 @@ numpydoc_xref_ignore = {
     "n_estimated_nodes",
     "n_samples",
     "n_variables",
+    "n_classes",
+    "NDArray",
 }
 numpydoc_xref_aliases = {
     # Networkx
@@ -261,4 +263,8 @@ html_context = {
 # resolve.
 
 nitpicky = True
-nitpick_ignore = []
+nitpick_ignore = [
+    ("py:class", "numpy._typing._generic_alias.ScalarType"),
+    ("py:class", "numpy._typing._array_like._SupportsArray"),
+    ("py:class", "numpy._typing._nested_sequence._NestedSequence"),
+]
