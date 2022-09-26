67376aaab99d67f2f79b87d15e4220d4c9a849a4
Adam Li <adam2392@gmail.com>
2022-09-26 13:26:01 -0400

    Address docstring comments
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- dodiscover/constraint/config.py
+++ dodiscover/constraint/config.py
@@ -2,7 +2,13 @@ from enum import Enum, EnumMeta
 
 
 class MetaEnum(EnumMeta):
+    """Meta class to enable easy checks for Enums."""
+
     def __contains__(cls, item):
+        """Allows 'contain' checks.
+
+        Example: ``is 'method' in EnumClass``.
+        """
         try:
             cls(item)
         except ValueError:
@@ -11,7 +17,14 @@ class MetaEnum(EnumMeta):
 
 
 class SkeletonMethods(Enum, metaclass=MetaEnum):
-    """Available methods for learning a skeleton from data."""
+    """Available methods for learning a skeleton from data.
+
+    Notes
+    -----
+    Allows 'contains' checks because of the metaclass. For example,
+    one can do ``is 'complete' in SkeletonMethods``, which would
+    return True.
+    """
 
     COMPLETE = "complete"
     NBRS = "neighbors"
