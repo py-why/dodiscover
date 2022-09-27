4a24f6fe1e6388a2a0b02e3c4e2382efd31a5cb7
Chris Trevino <darthtrevino@gmail.com>
2022-09-27 15:56:33 -0700

    format update
    Signed-off-by: Chris Trevino <darthtrevino@gmail.com>

--- dodiscover/context_builder.py
+++ dodiscover/context_builder.py
@@ -109,7 +109,7 @@ class ContextBuilder:
 
         self._observed_variables = observed
         self._latent_variables = latents
-        if (self._observed_variables is None):
+        if self._observed_variables is None:
             raise ValueError("Could not infer variables from data or given arguments.")
         return self
 
