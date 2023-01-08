34d6f5550e329beaf36ad8d5a046491d5700f15b
Adam Li <adam2392@gmail.com>
2023-01-08 15:17:19 -0500

    Adding cmi test
    Signed-off-by: Adam Li <adam2392@gmail.com>

---
+++ dodiscover/ci/utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+from numpy.typing import ArrayLike
+
+
+def _restricted_permutation(
+    nbrs: ArrayLike[np.int], n_shuffle_nbrs: int, n_samples: int, random_state=None
+) -> ArrayLike:
+    if random_state is None:
+        random_state = np.random.RandomState(seed=random_state)
+
+    # initialize the final permutation order
+    restricted_perm = np.zeros((n_samples,))
+
+    # generate a random order of samples to go through
+    random_order = random_state.permutation(n_samples)
+
+    # keep track of values we have already used
+    used = set()
+
+    # go through the random order
+    for idx in random_order:
+        m = 0
+        use_idx = nbrs[idx, m]
+
+        # if the current nbr is already used, continue incrementing
+        # until we have either found a new sample to use, or if
+        # we have reach the maximum number of shuffles to consider
+        while (use_idx in used) and (m < n_shuffle_nbrs - 1):
+            m += 1
+            use_idx = nbrs[idx, m]
+
+        restricted_perm[idx] = use_idx
+        used.add(use_idx)
+    return restricted_perm
