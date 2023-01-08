03f22cb296b580c247be9dac6fa8bc687fb60f86
specialized boy <41695878+HarshaSatyavardhan@users.noreply.github.com>
2023-01-09 03:44:22 +0530

    docstrings

--- dodiscover/replearning/gin.py
+++ dodiscover/replearning/gin.py
@@ -3,7 +3,6 @@ Wrapper for the GIN algorithm in causal-learn
 TODO: Need type hints
 """
 from causallearn.search.HiddenCausal.GIN.GIN import GIN as GIN_
-import pandas as pd
 import numpy.typing as npt
 from pywhy_graphs import CPDAG
 
@@ -11,11 +10,13 @@ class GIN:
     """
     Dodiscover wrapper for GIN in causal-learn package
     """
-    def __init__(self, indep_test_method="kci", alpha=0.05):
+    def __init__(self, indep_test_method="kci", alpha=0.05)-> None:
         """
         Using default parameters from GIN.
-        TODO: Add full set of GIN parameters with default
-        TODO: Add a base class
+        - indep_test_method: str, optional (default='kci')
+        The independence test method used by GIN.
+        - alpha: float, optional (default=0.05)
+        The significance level for the independence test.
         """
         self.graph_ = None # Should be in a base class
 
@@ -28,8 +29,31 @@ class GIN:
         self.causal_ordering = None
 
     def _causal_learn_to_pdag(self, cl_graph):
-        """"""
+        """Converts a causal-learn graph to a partially directed acyclic graph (CPDAG).
+    
+        Parameters:
+            - cl_graph (causal_learn Graph): Any
+            A causal-learn graph object.
+    
+        Returns:
+            - pdag: CPDAG
+            A CPDAG object.
+        """
         def _extract_edgelists(adj_mat: npt, names:str)-> tuple:
+            """Extracts directed and undirected edges from an adjacency matrix.
+        
+                Parameters:
+                    - adj_mat: numpy array
+                        The adjacency matrix of the graph.
+                    - names: list of str
+                        The names of the nodes in the graph.
+            
+                Returns:
+                - directed_edges: list of tuples
+                    The directed edges of the graph.
+                - undirected_edges: list of sets
+                    The undirected edges of the graph.
+            """
             directed_edges = []
             undirected_edges = []
             for i, row in enumerate(adj_mat):
@@ -52,7 +76,7 @@ class GIN:
         return pdag
 
 
-    def fit(self, data: pd.core.frame.DataFrame, context):
+    def fit(self, data, context):
         """Fit to data.
         TODO: How to apply context constraints?  Need to create issue"""
         causal_learn_graph, ordering = GIN_(
