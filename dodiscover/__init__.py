4ef0a228748543016a2d033bf24783ab25112fef
Adam Li <adam2392@gmail.com>
2023-04-05 22:07:30 -0700

    [ENH] Update to most recent API for learning skeleton -> enables PsiFCI and refactors Context object (#127)
    * Adding type changes
    * Update lock for pywhy graphs and networkx
    * Update learning skeleton API
    * Revamp Context object to support interventional causal assumptions
    
    ---------
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- dodiscover/__init__.py
+++ dodiscover/__init__.py
@@ -1,11 +1,8 @@
-"""
-DoDiscover - a library for Python-based Causal Discovery
-"""
-
-
+from . import cd  # noqa: F401
 from . import ci  # noqa: F401
 from . import metrics  # noqa: F401
 from ._protocol import EquivalenceClass, Graph
 from ._version import __version__  # noqa: F401
 from .constraint import FCI, PC
+from .context import Context
 from .context_builder import ContextBuilder, make_context
--- dodiscover/context.py
+++ dodiscover/context.py
@@ -1,12 +1,16 @@
-from typing import Any, Dict, Set
+from dataclasses import dataclass, field
+from typing import Any, Dict, FrozenSet, List, Set, Tuple
+from warnings import warn
 
 import networkx as nx
 
 from ._protocol import Graph
-from .typing import Column, NetworkxGraph
+from .base import BasePyWhy
+from .typing import Column
 
 
-class Context:
+@dataclass(eq=True, frozen=True)
+class Context(BasePyWhy):
     """Context of assumptions, domain knowledge and data.
 
     This should NOT be instantiated directly. One should instead
@@ -28,6 +32,12 @@ class Context:
         Included edges without direction.
     excluded_edges : nx.Graph
         Excluded edges without direction.
+    state_variables : Dict
+        Name of intermediate state variables during the learning process.
+    intervention_targets : list of tuple
+        List of intervention targets (known, or unknown), which correspond to
+        the nodes in the graph (known), or indices of datasets that contain
+        interventions (unknown).
 
     Raises
     ------
@@ -42,57 +52,43 @@ class Context:
     is used in conjunction with a discovery algorithm.
 
     Setting the a priori explicit direction of an edge is not supported yet.
+
+    **Testing for equality**
+
+    Currently, testing for equality is done on all attributes that are not
+    graphs. Defining equality among graphs is ill-defined, and as such, we
+    leave testing of the internal graphs to users. Some checks of equality
+    for example can be :func:`networkx.algorithms.isomorphism.is_isomorphic`
+    for checking isomorphism among two graphs.
     """
 
-    _variables: Set[Column]
-    _latents: Set[Column]
-    _init_graph: Graph
-    _included_edges: nx.Graph
-    _excluded_edges: nx.Graph
-    _state_variables: Dict[str, Any]
+    observed_variables: Set[Column]
+    latent_variables: Set[Column]
+    state_variables: Dict[str, Any]
+    init_graph: Graph = field(compare=False)
+    included_edges: nx.Graph = field(compare=False)
+    excluded_edges: nx.Graph = field(compare=False)
 
-    def __init__(
-        self,
-        variables: Set[Column],
-        latents: Set[Column],
-        init_graph: Graph,
-        included_edges: NetworkxGraph,
-        excluded_edges: NetworkxGraph,
-        state_variables: Dict[str, Any],
-    ) -> None:
-        # set to class
-        self._state_variables = state_variables
-        self._variables = variables
-        self._latents = latents
-        self._init_graph = init_graph
-        self._included_edges = included_edges
-        self._excluded_edges = excluded_edges
+    ########################################################
+    # for interventional data
+    ########################################################
+    # the number of distributions we expect to have access to
+    num_distributions: int = field(default=1)
 
-    @property
-    def included_edges(self) -> nx.Graph:
-        return self._included_edges
+    # whether or not observational distribution is present
+    obs_distribution: bool = field(default=True)
 
-    @property
-    def excluded_edges(self) -> nx.Graph:
-        return self._excluded_edges
+    # (optional) known intervention targets, corresponding to nodes in the graph
+    intervention_targets: List[Tuple[Column]] = field(default_factory=list)
 
-    @property
-    def init_graph(self) -> Graph:
-        return self._init_graph
+    # (optional) mapping F-nodes to their symmetric difference intervention targets
+    symmetric_diff_map: Dict[Any, FrozenSet] = field(default_factory=dict)
 
-    @property
-    def observed_variables(self) -> Set[Column]:
-        return self._variables
+    # sigma-map mapping F-nodes to their distribution indices
+    sigma_map: Dict[Any, Tuple] = field(default_factory=dict)
+    f_nodes: List = field(default_factory=list)
 
-    @property
-    def latent_variables(self) -> Set[Column]:
-        return self._latents
-
-    @property
-    def state_variables(self) -> Dict[str, Any]:
-        return self._state_variables
-
-    def add_state_variable(self, name: str, var: Any) -> None:
+    def add_state_variable(self, name: str, var: Any) -> "Context":
         """Add a state variable.
 
         Called by an algorithm to persist data objects that
@@ -105,22 +101,54 @@ class Context:
         var : any
             Any state variable.
         """
-        self._state_variables[name] = var
+        self.state_variables[name] = var
+        return self
 
-    def state_variable(self, name: str) -> Any:
+    def state_variable(self, name: str, on_missing: str = "raise") -> Any:
         """Get a state variable.
 
         Parameters
         ----------
         name : str
             The name of the state variable.
+        on_missing : {'raise', 'warn', 'ignore'}
+            Behavior if ``name`` is not in the dictionary of state variables.
+            If 'raise' (default) will raise a RuntimeError. If 'warn', will
+            raise a UserWarning. If 'ignore', will return `None`.
 
         Returns
         -------
         state_var : Any
             The state variable.
         """
-        if name not in self._state_variables:
-            raise RuntimeError(f"{name} is not a state variable: {self._state_variables}")
+        if name not in self.state_variables and on_missing != "ignore":
+            err_msg = f"{name} is not a state variable: {self.state_variables}"
+            if on_missing == "raise":
+                raise RuntimeError(err_msg)
+            elif on_missing == "warn":
+                warn(err_msg)
 
-        return self._state_variables[name]
+        return self.state_variables.get(name)
+
+    def copy(self) -> "Context":
+        """Create a deepcopy of the context."""
+        return Context(**self.get_params(deep=True))
+
+    ###############################################################
+    # Methods for interventional data.
+    ###############################################################
+    def get_non_f_nodes(self) -> Set:
+        """Get the set of non f-nodes."""
+        non_f_nodes = set()
+        f_nodes = set(self.f_nodes)
+        for node in self.init_graph.nodes:
+            if node not in f_nodes:
+                non_f_nodes.add(node)
+        return non_f_nodes
+
+    def reverse_sigma_map(self) -> Dict:
+        """Get the reverse sigma-map."""
+        reverse_map = dict()
+        for node, mapping in self.sigma_map.items():
+            reverse_map[mapping] = node
+        return reverse_map
