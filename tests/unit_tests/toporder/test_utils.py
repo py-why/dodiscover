c7229ee82228bfb2bd3c5945f1d307260dbf96f2
Nicholas Parente <parentenickj@gmail.com>
2024-09-06 01:11:44 -0400

    Handling all zero pairwise distances case
    Signed-off-by: Nicholas Parente <parentenickj@gmail.com>

--- tests/unit_tests/toporder/test_utils.py
+++ tests/unit_tests/toporder/test_utils.py
@@ -7,3 +7,7 @@ def test_kernel_width_when_zero_median_pairwise_distances():
     arr = np.zeros((100, 1), dtype=np.int64)
     arr[1] = 1
     assert kernel_width(arr) == 1
+
+def test_kernel_width_when_constant_pairwise_distances():
+    arr = np.ones((100, 1), dtype=np.int64)
+    assert kernel_width(arr) == 1
