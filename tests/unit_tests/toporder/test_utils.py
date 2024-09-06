744699e3ecd4633d4ed5d42942e37c90352a5f44
Nicholas Parente <parentenickj@gmail.com>
2024-09-06 10:45:46 -0400

    rename test function
    Signed-off-by: Nicholas Parente <parentenickj@gmail.com>

--- tests/unit_tests/toporder/test_utils.py
+++ tests/unit_tests/toporder/test_utils.py
@@ -8,6 +8,6 @@ def test_kernel_width_when_zero_median_pairwise_distances():
     arr[1] = 1
     assert kernel_width(arr) == 1
 
-def test_kernel_width_when_constant_pairwise_distances():
+def test_kernel_width_when_all_zero_pairwise_distances():
     arr = np.ones((100, 1), dtype=np.int64)
     assert kernel_width(arr) == 1
